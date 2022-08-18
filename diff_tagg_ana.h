f23af7d4b01be5cb7726d83689e70ece34eb6fed
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-08-18 14:56:46 -0400

    2nd try

--- diff_tagg_ana.h
+++ diff_tagg_ana.h
@@ -71,7 +71,7 @@ class diff_tagg_ana : public SubsysReco
 
   int process_PHG4Truth(PHCompositeNode* topNode);
   
-  // void getHEPMCTruth(PHCompositeNode *topNode);
+  void getHEPMCTruth(PHCompositeNode *topNode);
 
 // private:
 
@@ -93,7 +93,6 @@ class diff_tagg_ana : public SubsysReco
   int static_event_counter;
 
   //Truth
-  TH1D* h_pt;
 
   // Beam parameter
   Float_t e_beam_energy;
