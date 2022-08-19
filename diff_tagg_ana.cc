1d2f22e95f21b6c3c849f50f3e2c85171ac9434e
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-08-19 10:37:56 -0400

    ok stop because SvtxTrackMap SEEMS not working.

--- diff_tagg_ana.cc
+++ diff_tagg_ana.cc
@@ -374,7 +374,7 @@ void diff_tagg_ana::getHEPMCTruth(PHCompositeNode *topNode)
 void diff_tagg_ana::getTracks(PHCompositeNode *topNode)
 {
   /// SVTX tracks node
-  SvtxTrackMap_v1 *trackmap = findNode::getClass<SvtxTrackMap_v1>(topNode, "SvtxTrackMap_v1");
+  SvtxTrackMap *trackmap = findNode::getClass<SvtxTrackMap>(topNode, "SvtxTrackMap");
 
   if (!trackmap)
   {
@@ -403,7 +403,7 @@ void diff_tagg_ana::getTracks(PHCompositeNode *topNode)
   {
     cout << "Get the SVTX tracks" << endl;
   }
-  for (SvtxTrackMap_v1::Iter iter = trackmap->begin();
+  for (SvtxTrackMap::Iter iter = trackmap->begin();
        iter != trackmap->end();
        ++iter)
   {
