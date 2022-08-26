fcba22f8e24c0290f7b86176fd86319ab2977794
Kong Tu <kongkong@rice.edu>
2022-08-26 10:09:03 -0400

    add

--- macros/copy.sh
+++ macros/copy.sh
@@ -1,3 +1,6 @@
+echo "copying 1 file of diffractive phi from jlab thru xrood"
+
 export LD_PRELOAD=/usr/lib64/libXrdPosixPreload.so
 cp root://dtn-eic.jlab.org//work/eic2/EPIC/Campaigns/prop.8/ExclDiffTagg/Sartre/eAu-18x110-coh_phi-IP6/DST_ExclDiffTagg_Sartre_eAu-18x110-coh_phi-IP6_000_0182000_02000.root ../DSTrees/test.root
 
+echo "now it lives under ../DSTrees and named test.root"
