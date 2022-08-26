2bc56955224a13359a1cca55e0e645b28beb1db2
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-08-25 20:57:16 -0400

    next level is to add FFs

--- diff_tagg_ana.h
+++ diff_tagg_ana.h
@@ -158,7 +158,7 @@ class diff_tagg_ana : public SubsysReco
   std::string outfilename;
   Fun4AllHistoManager *hm;
 
-  TFile *outfile;mv 
+  TFile *outfile;
   TTree* m_eventtree;
   TTree *m_truthtree;
   TTree *m_tracktree;
